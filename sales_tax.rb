9b78ba251df12bd93efdc880d7c681da8a995c97
Filip Stankovic <filips@gmail.com>
2016-01-14 21:06:18 -0500

    Attempt to calculate price for importgood

--- sales_tax.rb
+++ sales_tax.rb
@@ -17,20 +17,28 @@ class Good
 end
 
 class ImportGood < Good
-  def initialize
-    super
+
+  def initialize(price, type_of_good)
+    super(price, type_of_good)
   end
 
   def calculate_price
-    super
+    regular_price = super
+    regular_price += (@price * 0.05)
+    puts regular_price
+    #puts "$#{sprintf('%.2f', regular_price)}"
   end
 end
 
 def testmethod
   good1 = Good.new(8, "Book")
   good2 = Good.new(8, "Perfume")
+  good3 = ImportGood.new(8, "Book")
+  good4 = ImportGood.new(8, "Perfume")
   good1.calculate_price
   good2.calculate_price
+  good3.calculate_price
+  good4.calculate_price
 end
 
 testmethod
