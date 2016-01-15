99943ad90324732508718015bc70f164b6111d5c
Filip Stankovic <filips@gmail.com>
2016-01-14 20:53:32 -0500

    Fixing tax variable

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,22 +1,29 @@
 class Good
   attr_accessor :price
 
+  @@tax = 1.15
+
   def initialize (price, type_of_good)
     @price = price
-    @TAX = 1.15
     @type_of_good = type_of_good
     @exempt = ["Book", "Food", "Medical"].include?(@type_of_good)
   end
 
   def calculate_price
-    @exempt ? (@price) : (@price *= @TAX)
+    @exempt ? (@price) : (@price *= @@tax)
     puts "$#{sprintf('%.2f', @price)}"
   end
 
 end
 
 class ImportGood < Good
+  def initialize
+    super
+  end
 
+  def calculate_price
+    super
+  end
 end
 
 def testmethod
